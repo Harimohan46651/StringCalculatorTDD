1bdd093672da70812cadd525bbe972ca794e7274
Harimohancantilever <hari@cantileverlabs.com>
2025-10-01 00:58:47 +0530

    code change to support different delimiters

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,7 +1,13 @@
 class StringCalculator {
   add(numbers){
     if (numbers === "") return 0;
-    const parts = numbers.split(/[\n,]/);
+    let delimiter = /[\n,]/;
+    if (numbers.startsWith("//")) {
+      const parts = numbers.split("\n");
+      delimiter = new RegExp(parts[0].slice(2)); 
+      numbers = parts[1];
+    }
+    const parts = numbers.split(delimiter);
     console.log(parts)
     if (parts.length === 1) return parseInt(numbers);
     let sum = 0;
