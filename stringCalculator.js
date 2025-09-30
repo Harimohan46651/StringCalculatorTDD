376788c9002a67855af4c58f955a25bafbd3b023
Harimohancantilever <hari@cantileverlabs.com>
2025-10-01 01:12:17 +0530

    code change for negative numbers

--- stringCalculator.js
+++ stringCalculator.js
@@ -9,6 +9,9 @@ class StringCalculator {
     }
     const parts = numbers.split(delimiter);
     console.log(parts)
+    let negativeNumbers = parts.filter((part) => parseInt(part) < 0);
+    console.log(negativeNumbers)
+    if(negativeNumbers.length > 0) throw new Error("negative numbers not allowed " + negativeNumbers.join(","))
     if (parts.length === 1) return parseInt(numbers);
     let sum = 0;
     for(let i=0;i<parts.length;i++){
